930d30ea4b8557830fdb979a517d709f4b467c93
moonlomon <sjmun0420@gmail.com>
2024-08-23 07:27:40 +0900

    유닛19 일부

---
+++ unit19_별출력_중첩루프/test.py
@@ -0,0 +1,16 @@
+# for i in range(5):
+#     for j in range(5):
+#         print('j:',j,sep='',end=' ')
+#     print('i:',i,sep='')
+
+x = int(input())
+
+for i in range(x):
+    for j in range(x):
+        if :
+            print('*', end='')
+        else:
+            print(" ", end='')
+    print()
+
+
