82eb6f2828489222dee1d6bfd6857a7d4611c86d
moonlomon <sjmun0420@gmail.com>
2024-08-24 07:30:16 +0900

    유닛19 md작성 완료

--- unit19_별출력_중첩루프/test.py
+++ unit19_별출력_중첩루프/test.py
@@ -1,16 +1,8 @@
-# for i in range(5):
-#     for j in range(5):
-#         print('j:',j,sep='',end=' ')
-#     print('i:',i,sep='')
-
-x = int(input())
-
-for i in range(x):
-    for j in range(x):
-        if :
+x = 5
+for i in range(5):
+    for j in range(5):
+        if i+j==x-1:
             print('*', end='')
         else:
-            print(" ", end='')
+            print(' ', end='')
     print()
-
-
